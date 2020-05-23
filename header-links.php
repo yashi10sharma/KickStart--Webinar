10c000dd6e3324c12905e2af9161da3868f7883f
Yash kumar shukla <39242081+21170293@users.noreply.github.com>
2020-05-23 23:03:11 +0530

    Update header-links.php

--- header-links.php
+++ header-links.php
@@ -1,7 +1,12 @@
 <title>@ KickStart Webinar</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="script/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <style>
             .icon-bar{
                 background-color:lightseagreen ;
